7016204afa2be06ba88536318737c2236297ae6d
Ben Thackray <mthack92@gmail.com>
2021-06-29 21:06:02 -0400

    added the modules and holy moly. Also, added more logic to index.js, finished starter html and css

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const js = require("js");
+const js = require("fs");
 const inquirer = require('inquirer');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./Employee");
+const Employee = require("./lib/Employee");
+const { writeFile } = require("fs");
 
 function newEmployee() {
     inquirer.prompt([{
@@ -30,6 +31,81 @@ function newEmployee() {
             "Intern",
         ],
         name: 'role',
-    },
+    }
     ])
+
+        .then(function ({ name, id, email, role }) {
+            if (role === 'Manager') {
+                inquirer.prompt([{
+                    type: 'input',
+                    message: 'What is their office number?',
+                    name: 'office'
+                }])
+            } else if (role === 'Engineer') {
+                inquirer.prompt([{
+                    type: 'input',
+                    message: 'What is their GitHub username?',
+                    name: 'github'
+                }])
+            } else {
+                inquirer.prompt([{
+                    type: 'input',
+                    message: 'What school do they attend?',
+                    name: 'school'
+                }])}
+        })
+
 }
+
+
+
+const startHTML = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="../dist/style.css" rel="stylesheet">
+    <title>Team Profile</title>
+</head>
+
+<body>
+    <div class="jumbotron">
+        <h1>Team Profiles</h1>
+    </div>
+
+
+    <div class="team-cards">`
+
+const managerHTML = `        <article>
+<h3>${this.name}</h3>
+<p class="manager">Manager</p>
+<p>ID: ${this.id}</p>
+<p>Email: ${this.email} </p>
+<p>Office Number: ${this.office} </p>
+</article>`
+
+const engineerHTML = `        <article>
+<h3>${this.name}</h3>
+<p class="engineer">Engineer</p>
+<p>ID: ${this.id}</p>
+<p>Email: ${this.email} </p>
+<p>GitHub: ${this.github} </p>
+</article>`
+
+const internHTML = `        <article>
+<h3>${this.name}</h3>
+<p class="intern">Intern</p>
+<p>ID: ${this.id}</p>
+<p>Email: ${this.email} </p>
+<p>School: ${this.school} </p>
+</article>`
+
+const finishHTML = `    </div>
+</body>
+
+</html>`
+
+newEmployee()
